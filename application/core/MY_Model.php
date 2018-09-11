9e22b09afc4f8466c57eabac7d1d504da6194def
Arie Kurnia Ramdhani <ak.ramdhani@gmail.com>
2018-09-11 13:29:11 +0700

    Change to HMVC

---
+++ application/core/MY_Model.php
@@ -0,0 +1,16 @@
+<?php
+(defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/**
+ * Description of MY_model
+ *
+ * @author : ak.ramdhani@gmail.com
+ * @Class Name : MY_model
+ * @Table Name : ''
+ *
+ */
+ 
+ class MY_model extends CI_Model {
+
+     
+}   
